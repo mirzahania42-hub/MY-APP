bfe3364ea2a18675ae414e0aa1688a1e8acc85d3
hania <hania@hanias-MacBook-Air.local>
2026-04-07 14:41:18 +0530

    Initial commit

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
+
 
 @app.route("/")
 def home():
     return jsonify(message="Hello DevOps Engg")
 
+
 @app.route("/health")
 def health():
     return jsonify(status="OK")
 
-if __name__ == "__main__":
+
+if _name_ == "_main_":
     app.run(host="0.0.0.0", port=5000)
